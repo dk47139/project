f586925d8bbd947f7a80ff2827d9ce7a6f712718
Dmitry <example@gmail.com>
2021-06-22 12:17:12 +0700

    qqq

---
+++ script.js
@@ -0,0 +1,30 @@
+
+const number0fFilms = +prompt('Сколько фильмов вы посмотрели?', '');
+
+
+
+
+
+const personalMovieDB = {
+    count: number0fFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+
+
+
+const a = prompt('Один из последних просмотренных фильмов?', ''),
+      b = prompt('На сколько оцените его?', ''),
+      c = prompt('Один из последних просмотренных фильмов?', ''),
+      d = prompt('На сколько оцените его?', '');
+
+personalMovieDB.movies.a = b;
+personalMovieDB.movies.c = d;
+
+console.log(personalMovieDB);
+
+
+
